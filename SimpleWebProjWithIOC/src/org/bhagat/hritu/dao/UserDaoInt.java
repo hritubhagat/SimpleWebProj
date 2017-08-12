b461579f341e6e3fc01719a1f5434ffed70a12a8
hritubhagat <hritubhagat@gmail.com>
2017-08-12 21:29:30 +0530

    add method added

--- SimpleWebProjWithIOC/src/org/bhagat/hritu/dao/UserDaoInt.java
+++ SimpleWebProjWithIOC/src/org/bhagat/hritu/dao/UserDaoInt.java
@@ -5,4 +5,7 @@ import org.bhagat.hritu.dto.UserDTO;
 public interface UserDaoInt {
 	public UserDTO findByLogin(UserDTO dto) throws Exception;
 	public UserDTO findByPK(UserDTO dto) throws Exception;
-	public void update(UserDTO dto) throws Exception;}
+	public void update(UserDTO dto) throws Exception;
+	public void add(UserDTO dto) throws Exception;
+	
+}
